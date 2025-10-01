b8334dbc0109ab3b9adaa5aef19702b3f0a705c2
Galih1004 <mochgalihputrapratama@gmail.com>
2025-10-01 10:34:57 +0700

    Percobaan 1

---
+++ Jobsheet5/ifCetakKRS14.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ifCetakKRS14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("---Cetak KRS Siakad---");
+        System.out.print("Apakah UKT sudah lunas (true/false)? ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT telat terverifikasi");
+            System.out.print("Silahkan cetak KRS dan minta tanda tangan DPA ");
+            
+            
+        } else {
+            System.out.println("UKT belum lunas, silakan lunasi UKT terlebih dahulu");
+        }
+        
+        
+        
+        sc.close();
+    }
+}
