099baf711c49a7e894322a3f51b99fc1ba7dc390
Galih1004 <mochgalihputrapratama@gmail.com>
2025-10-08 10:15:42 +0700

    Percobaan running menggunakan nilai huruf

--- CaseMethod14/CaseMethod1.java
+++ CaseMethod14/CaseMethod1.java
@@ -34,6 +34,10 @@ public class CaseMethod1 {
         double nilaiAkhir1 = (double) (0.3 * uts1 + 0.4 * uas1 + 0.3 * tugas1);
         double nilaiAkhir2 = (double) (0.3 * uts2 + 0.4 * uas2 + 0.3 * tugas2);
 
+        String huruf1 = getNilaiHuruf(nilaiAkhir1);
+        String status1 = nilaiAkhir1 >= 79 ? "LULUS" : "TIDAK LULUS";
+        String huruf2 = getNilaiHuruf(nilaiAkhir2);
+        String status2 = nilaiAkhir2 >= 79 ? "LULUS" : "TIDAK LULUS";
 
     // Output Data Mahasiswa, Nilai Mata Kuliah 1
         System.out.println("\n=== Hasil Mahasiswa ===");
@@ -50,11 +54,39 @@ public class CaseMethod1 {
         System.out.println("Nilai UAS: " + uas2);
         System.out.println("Nilai Tugas: " + tugas2);
 
-        System.out.println("Akumulasi Nilai Akhir:");
+        System.out.println("Memunculkan Nilai Akhir:");
         System.out.println("Nilai Akhir Mata Kuliah 1: " + nilaiAkhir1);
-        System.out.println("Nilai Akhir Mata Kuliah 2: " + nilaiAkhir2);    
+        System.out.println("Nilai Akhir Mata Kuliah 2: " + nilaiAkhir2);
+        System.out.println("Nilai Huruf Mata Kuliah 1: " + huruf1);
+        System.out.println("Status Mata Kuliah 1: " + status1);
+        System.out.println("Nilai Huruf Mata Kuliah 2: " + huruf2);
+        System.out.println("Status Mata Kuliah 2: " + status2);    
 
         
      sc.close();
     } 
-}
+    // Method untuk mengubah nilai akhir menjadi nilai huruf
+    public static String getNilaiHuruf(double nilaiAkhir) {
+        if (nilaiAkhir >= 85) {
+            return "A";
+        } else if (nilaiAkhir >= 80) {
+            return "A-";
+        } else if (nilaiAkhir >= 75) {
+            return "B+";
+        } else if (nilaiAkhir >= 70) {
+            return "B";
+        } else if (nilaiAkhir >= 65) {
+            return "B-";
+        } else if (nilaiAkhir >= 60) {
+            return "C+";
+        } else if (nilaiAkhir >= 55) {
+            return "C";
+        } else if (nilaiAkhir >= 50) {
+            return "D";
+        } else {
+            return "E";
+        }
+    }
+}     
+
+
