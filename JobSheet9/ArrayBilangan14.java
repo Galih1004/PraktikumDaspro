71d821f464c8816f454101d3669a83b3423d1e4a
Galih1004 <mochgalihputrapratama@gmail.com>
2025-11-12 17:37:01 +0700

    commit ( modifikasi percobaan menjawab pertanyaan 3)

--- JobSheet9/ArrayBilangan14.java
+++ JobSheet9/ArrayBilangan14.java
@@ -4,11 +4,8 @@ public class ArrayBilangan14 {
     public static void main(String[] args) {
         int[] bil = {5, 13, -7, 17};
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-
+        for (int i = 0; i < bil.length; i++) {
+            System.out.println("Bilangan pada indeks ke-" + i + " adalah " + bil[i]);
+        }
     }
-    
 }
--- JobSheet9/MenjawabPertanyaanPercobaan.md
+++ JobSheet9/MenjawabPertanyaanPercobaan.md
@@ -1,3 +1,4 @@
 // Jawaban Pada Percobaan 1
 1. Error, karena format array adalah int[] bukan double[] karena tipe data int hanya bisa menyimpan data dengan bilangan bulat.
 2. sudah
+3. 
